4cccabd3e645ad9d09563a0b17704e0cf875fb01
CesarCid <cesaraugustoap@gmail.com>
2021-06-06 00:14:53 -0300

    Match 3 evaluation methods and debugs;

--- Assets/Scripts/Gameplay/GameBoard.cs
+++ Assets/Scripts/Gameplay/GameBoard.cs
@@ -1,90 +1,220 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameBoard : MonoBehaviour
+namespace AVMT.Gameplay
 {
-    private static int minMatch = 3;
-
-    public static float SlotSize = 2.56f;
-    public static float SlotHalfSize = 1.28f;
-
     public enum Direction { Right, Left, Up, Down };
-    public enum Coord { Horizontal, Vertical }
 
-    public static Vector2Int BoardLenght = new Vector2Int(6, 9);
-
-    [SerializeField]
-    private GameObject boardSlotPrefab;
-
-    [SerializeField]
-    private Transform boardPivot;
-    public Transform BoardPivot => boardPivot;
-
-    private BoardSlot[,] slots = new BoardSlot[BoardLenght.x , BoardLenght.y];
-    public BoardSlot[,] Slots { get => slots; }
-
-    private void Awake()
+    public class GameBoard : MonoBehaviour
     {
-        SetupSlots();
-    }
-    private void SetupSlots()
-    {
-        for (int y = 0; y < BoardLenght.y; y++)
+        private static int minMatch = 3;
+
+        public static float SlotSize = 2.56f;
+        public static float SlotHalfSize = 1.28f;
+
+        private static Vector2Int[] directionVector = new Vector2Int[4] { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
+        private static Direction[] oppositeDirections = new Direction[4] { Direction.Left, Direction.Right, Direction.Down, Direction.Up };
+
+        public enum Axis { Horizontal, Vertical }
+        public static Vector2Int BoardLenght = new Vector2Int(6, 9);
+
+        [SerializeField]
+        private GameObject boardSlotPrefab;
+
+        [SerializeField]
+        private Transform boardPivot;
+        public Transform BoardPivot => boardPivot;
+
+        private BoardSlot[,] slots = new BoardSlot[BoardLenght.x, BoardLenght.y];
+        public BoardSlot[,] Slots { get => slots; }
+
+        private void Awake()
         {
-            for (int x = 0; x < BoardLenght.x; x++)
-            {
-                GameObject slot = Instantiate(boardSlotPrefab, boardPivot);
-                slot.transform.localPosition = new Vector3(SlotSize * x, SlotSize * y, 0);
-                slot.name = "BoardSlot " + x + "," + y;
-                slots[x,y] = slot.GetComponent<BoardSlot>();
-                slots[x,y].index = new Vector2Int(x, y);
-            }
+            SetupSlots();
         }
-    }
-
-    public bool EvaluateMatch(BoardSlot slot)
-    {
-        int matchCount = EvaluateMatchInDirection(slot, Direction.Down);
-
-        return matchCount >= minMatch;
-    }
-
-    private int EvaluateMatchInDirection(BoardSlot slot, Direction direction)
-    {
-        Coord coordinate = (direction == Direction.Right || direction == Direction.Left) ? Coord.Horizontal : Coord.Vertical;
-
-        //slot.index.x + 1;
-        int startingPoint = (coordinate == Coord.Horizontal) ? slot.index.x : slot.index.y;
-        startingPoint += (direction == Direction.Right || direction == Direction.Up) ? 1 : -1;
-        int endPoint = (coordinate == Coord.Horizontal) ? BoardLenght.x : BoardLenght.y;
-        int slotX = slot.index.x;
-        int slotY = slot.index.y;
-
-        int matchCount = 1;
-
-        //TODO: Left and Down are not working for i is always incrementing;
-        for (int i = startingPoint; i < endPoint; i++)
+        private void SetupSlots()
         {
-            slotX = (coordinate == Coord.Horizontal) ? i : slotX;
-            slotY = (coordinate == Coord.Horizontal) ? slotY : i;
+            for (int y = 0; y < BoardLenght.y; y++)
+            {
+                for (int x = 0; x < BoardLenght.x; x++)
+                {
+                    GameObject slot = Instantiate(boardSlotPrefab, boardPivot);
+                    slot.transform.localPosition = new Vector3(SlotSize * x, SlotSize * y, 0);
+                    slot.name = "BoardSlot " + x + "," + y;
+                    slots[x, y] = slot.GetComponent<BoardSlot>();
+                    slots[x, y].index = new Vector2Int(x, y);
+                }
+            }
+        } 
 
-            if (slots[slotX, slotY].Piece.Type == slot.Piece.Type)
+        public bool GetMatchesInColumnAfterMovement(Vector2Int slot, Direction movementDirection, out List<List<int>> matchIndexes) 
+        {
+            Vector2Int movedSlot = slot + GetDirectionVector(movementDirection);
+            int columnToLook = movedSlot.x;
+
+            if(columnToLook < 0 || columnToLook > BoardLenght.x)
             {
-                matchCount++;                
+                matchIndexes = new List<List<int>>();
+                return false;
             }
-            else
+
+            GamePieceType[] column = GetColumnTypes(columnToLook);
+
+            GamePieceType movingPieceType = slots[slot.x, slot.y].Piece.Type;
+
+            if (GetDirectionAxis(movementDirection) == Axis.Vertical)
             {
-                break;
+                GamePieceType targetPieceType = column[movedSlot.y];
+                column[slot.y] = targetPieceType;
             }
+
+            column[movedSlot.y] = movingPieceType;
+
+            return EvaluateMatch(column, out matchIndexes);
         }
 
-        return matchCount;
-    }
+        public bool GetMatchesInLineAfterMovement(Vector2Int slot, Direction movementDirection, out List<List<int>> matchIndexes)
+        {
+            Vector2Int movedSlot = slot + GetDirectionVector(movementDirection);
+            int lineToLook = movedSlot.y;
 
-    int Increment(int i) 
-    {
-        return i++;
+            if (lineToLook < 0 || lineToLook > BoardLenght.y)
+            {
+                matchIndexes = new List<List<int>>();
+                return false;
+            }            
+
+            GamePieceType[] line = GetLineTypes(lineToLook);
+
+            GamePieceType movingPieceType = slots[slot.x, slot.y].Piece.Type;
+            
+            if (GetDirectionAxis(movementDirection) == Axis.Horizontal)
+            {
+                GamePieceType targetPieceType = line[movedSlot.x];
+                line[slot.x] = targetPieceType;
+            }
+
+            line[movedSlot.x] = movingPieceType;
+
+            return EvaluateMatch(line, out matchIndexes);
+        }
+
+        public bool GetMatchesInColumn (int columnIndex, out List<List<int>> matchIndexes)
+        {
+            return EvaluateMatch(GetColumnTypes(columnIndex), out matchIndexes);
+        }
+
+        public bool GetMatchesInLine(int lineIndex, out List<List<int>> matchIndexes)
+        {
+            return EvaluateMatch(GetLineTypes(lineIndex), out matchIndexes);
+        }
+
+        private bool EvaluateMatch(GamePieceType[] sequence, out List<List<int>> matchIndexes)
+        {
+            int matchCount = 1;
+            matchIndexes = new List<List<int>>();
+
+            bool matchFound = false;
+            int firstFoundIndex = 0;
+            GamePieceType lastFoundType = null;
+
+            //TODO: change to a "look-ahead for" to prevent code copying outside the for
+            for (int index = 0; index < sequence.Length; index++) 
+            {
+                if (lastFoundType == sequence[index])
+                {
+                    matchCount++;
+                    if (firstFoundIndex == 0)
+                    {
+                        firstFoundIndex = index - 1;
+                    }
+                }
+                else 
+                {
+                    if (matchCount >= minMatch)
+                    {
+                        List<int> match = new List<int>();
+                        for (int matchIndex = firstFoundIndex; matchIndex < firstFoundIndex + matchCount; matchIndex++)
+                        {
+                            match.Add(matchIndex);
+                        }
+                        matchIndexes.Add(match);
+                        matchFound = true;
+                    }
+
+                    matchCount = 1;
+                    firstFoundIndex = 0;
+                }
+
+                lastFoundType = sequence[index];
+            }
+
+            if (matchCount >= minMatch)
+            {
+                List<int> match = new List<int>();
+                for (int matchIndex = firstFoundIndex; matchIndex < firstFoundIndex + matchCount; matchIndex++)
+                {
+                    match.Add(matchIndex);
+                }
+                matchIndexes.Add(match);
+                matchFound = true;
+            }
+
+            return matchFound;
+        }
+
+        private GamePieceType[] GetColumnTypes (int columnIndex)
+        {
+            int verticalLenght = slots.GetLength((int)Axis.Vertical);
+
+            GamePieceType[] column = new GamePieceType[verticalLenght];
+
+            for (int i = 0; i < verticalLenght; i++) 
+            {
+                /*if(slots[columnIndex, i].Piece == null)
+                {
+                    break;
+                }*/
+
+                column[i] = slots[columnIndex, i].Piece.Type;
+            }
+
+            return column;
+        }
+
+        private GamePieceType[] GetLineTypes(int lineIndex)
+        {
+            int horizontalLenght = slots.GetLength((int)Axis.Horizontal);
+
+            GamePieceType[] line = new GamePieceType[horizontalLenght];
+
+            for (int i = 0; i < horizontalLenght; i++)
+            {
+                /*if (slots[i, lineIndex].Piece == null)
+                {
+                    break;
+                }*/
+
+                line[i] = slots[i, lineIndex].Piece.Type;
+            }
+
+            return line;
+        }
+
+        public static Vector2Int GetDirectionVector (Direction direction)
+        {
+            return directionVector[(int)direction];
+        }
+
+        public static Direction GetOppositeDiretion(Direction direction)
+        {
+            return oppositeDirections[(int)direction];
+        }
+
+        public static Axis GetDirectionAxis (Direction direction)
+        {
+            return (direction == Direction.Right || direction == Direction.Left) ? Axis.Horizontal : Axis.Vertical;
+        }
     }
 }
--- Assets/Scripts/Gameplay/GamePiecesController.cs
+++ Assets/Scripts/Gameplay/GamePiecesController.cs
@@ -1,75 +1,129 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class GamePiecesController : MonoBehaviour
-{    
-    [SerializeField]
-    private GamePieceType[] gamePieces;
-
-    [SerializeField]
-    private GameBoard gameBoard;
-
-    private TouchController touchController;
-       
-
-    void Awake()
+namespace AVMT.Gameplay
+{
+    public class GamePiecesController : MonoBehaviour
     {
-        touchController = TouchController.Instance;
+        [SerializeField]
+        private GamePieceType[] gamePieces;
 
-        touchController.TouchedDownOnInteractable += OnPieceTouchDown;
-        touchController.TouchedUpOnInteractable += OnPieceTouchUp;
-    }
+        [SerializeField]
+        private GameBoard gameBoard;
 
-    void Start() 
-    {
-        PopulateBoard();
-    }
+        private TouchController touchController;
 
-    public void PopulateBoard()
-    {
-        for (int y = 0; y < GameBoard.BoardLenght.y; y++)
+        private void OnValidate()
         {
-            for (int x = 0; x < GameBoard.BoardLenght.x; x++)
+            if (gameBoard != null)
             {
-                CreatePiece(GetRandomPieceType(), gameBoard.Slots[x,y]);
+                return;
+            }
+
+            gameBoard = FindObjectOfType<GameBoard>();
+        }
+
+        void Awake()
+        {
+            touchController = TouchController.Instance;
+
+            touchController.TouchedDownOnInteractable += OnPieceTouchDown;
+            touchController.TouchedUpOnInteractable += OnPieceTouchUp;
+        }
+
+        public void PopulateBoard()
+        {
+            for (int y = 0; y < GameBoard.BoardLenght.y; y++)
+            {
+                for (int x = 0; x < GameBoard.BoardLenght.x; x++)
+                {
+                    List<GamePieceType> exceptions = new List<GamePieceType>();
+                    /*
+                    if (gameBoard.GetMatchesInLine(y, out List<List<int>> lineMatches))
+                    {
+                        foreach(List<int> list in lineMatches)
+                        {
+                            exceptions.Add(gameBoard.Slots[list[0], y].Piece.Type);
+                        }
+                    }
+
+                    if (gameBoard.GetMatchesInColumn(x, out List<List<int>> columnMatches))
+                    {
+                        foreach (List<int> list in columnMatches)
+                        {
+                            exceptions.Add(gameBoard.Slots[x, list[0]].Piece.Type);
+                        }
+                    }
+                    */
+                    CreatePiece(GetRandomPieceType(exceptions), gameBoard.Slots[x, y]);
+                }
             }
         }
-    }
 
-    private GamePieceType GetRandomPieceType() 
-    {
-        return GetRandomPieceType(new GamePieceType[0]);
-    }
-    private GamePieceType GetRandomPieceType(GamePieceType[] exceptions)
-    {
-        IEnumerable<GamePieceType> availablePieces = gamePieces.Except(exceptions);
-
-        return availablePieces.ElementAt(Random.Range(0, availablePieces.Count()));
-    }
-
-
-    private GameObject CreatePiece (GamePieceType type, BoardSlot slotParent)
-    {            
-        GameObject piece = GameObject.Instantiate(type.Prefab, slotParent.transform);
-        slotParent.Piece = piece.GetComponent<GamePiece>();
-        return piece;
-    }
-
-    private void OnPieceTouchDown(GameObject go) 
-    {
-        Debug.Log("[GamePiecesController] OnPieceTouchDown | piece: " + go.name);        
-    }
-
-    private void OnPieceTouchUp(GameObject go) 
-    {
-        Debug.Log("[GamePiecesController] OnPieceTouchUp | piece: " + go.name);
-        BoardSlot slot = go.GetComponent<BoardSlot>();
-
-        if (slot != null)
+        private GamePieceType GetRandomPieceType()
         {
-            Debug.Log("[GamePiecesController] OnPieceTouchDown | Evaluate right: " + gameBoard.EvaluateMatch(slot));
+            return GetRandomPieceType(new List<GamePieceType>());
+        }
+        private GamePieceType GetRandomPieceType(List<GamePieceType> exceptions)
+        {
+            IEnumerable<GamePieceType> availablePieces = gamePieces.Except(exceptions);
+
+            return availablePieces.ElementAt(Random.Range(0, availablePieces.Count()));
+        }
+
+
+        private GameObject CreatePiece(GamePieceType type, BoardSlot slotParent)
+        {
+            GameObject piece = GameObject.Instantiate(type.Prefab, slotParent.transform);
+            slotParent.Piece = piece.GetComponent<GamePiece>();
+            return piece;
+        }
+
+        private void OnPieceTouchDown(GameObject go)
+        {
+            Debug.Log("[GamePiecesController] OnPieceTouchDown | piece: " + go.name);
+        }
+
+        private void OnPieceTouchUp(GameObject go)
+        {
+            Debug.Log("[GamePiecesController] OnPieceTouchUp | piece: " + go.name);
+            BoardSlot slot = go.GetComponent<BoardSlot>();
+
+            if (slot != null)
+            {
+                List<List<int>> matches;
+                if (gameBoard.GetMatchesInColumn(slot.index.x, out matches)) 
+                {
+                    Debug.Log(matches.Count + " matches");
+                }
+
+                if (gameBoard.GetMatchesInColumnAfterMovement(slot.index, Direction.Right, out matches))
+                {
+                    Debug.Log(matches.Count + " column matches after moving to the right");
+                }
+
+                if (gameBoard.GetMatchesInLineAfterMovement(slot.index, Direction.Right, out matches))
+                {
+                    Debug.Log(matches.Count + " line matches after moving to the right");
+                }
+
+                if (gameBoard.GetMatchesInColumnAfterMovement(slot.index, Direction.Up, out matches))
+                {
+                    Debug.Log(matches.Count + " column matches after moving up");
+                }
+
+                if (gameBoard.GetMatchesInLineAfterMovement(slot.index, Direction.Up, out matches))
+                {
+                    Debug.Log(matches.Count + " line matches after moving up");
+                }
+
+                //foreach (var result in gameBoard.EvaluateMatchesInAllDirections(slot.index))
+                //    Debug.Log(result);
+                //Debug.Log(gameBoard.EvaluatePossibleMatchesInAllNeighbours(slot.index, slot.Piece.Type));
+
+
+            }
         }
     }
 }
