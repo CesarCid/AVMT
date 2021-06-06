345c7408c1d1b8522bfb8a9cd70b9d7a346363e7
CesarCid <cesaraugustoap@gmail.com>
2021-06-06 00:14:12 -0300

    Small Gameplay UI setup;

---
+++ Assets/Scripts/Gameplay/RoundManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AVMT.Gameplay
+{
+    public class RoundManager : MonoBehaviour
+    {
+        private int currentRound = 0;
+        public int CurrentRound { get => currentRound; }
+
+        [SerializeField]
+        private GamePiecesController piecesController;
+
+
+        private void OnValidate()
+        {
+            if (piecesController != null) 
+            {
+                return;
+            }
+
+            piecesController = FindObjectOfType<GamePiecesController>();
+        }
+
+        private void Start()
+        {
+            StartRound();
+        }
+        private void StartRound() 
+        {
+            piecesController.PopulateBoard();
+        }
+    }
+}
